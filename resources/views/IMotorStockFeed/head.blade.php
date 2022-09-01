178e1ebe78886beed4c352db4cb11a8253d934ab
Sherin <sl@Sherins-Mac-mini.local>
2022-09-01 15:14:48 +0530

    Added GA tracking ID and sitemap

--- resources/views/IMotorStockFeed/head.blade.php
+++ resources/views/IMotorStockFeed/head.blade.php
@@ -1,5 +1,5 @@
 <html>
 	<head>
-		<meta content="https://www.googletagmanager.com/gtag/js?id=G-PECXMRCRRF"/>
+		<meta content="https://www.googletagmanager.com/gtag/js?id=G-X9M317QS0L"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
